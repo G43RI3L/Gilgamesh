0c44d23db9d5e88a57b048d05b38f2c36472a264
Gabriel Higor <127054780+G43RI3L@users.noreply.github.com>
2025-10-19 00:07:17 -0300

    Change QR reader import to react-qr-reader-es6

--- frontend/src/components/QRScanner.js
+++ frontend/src/components/QRScanner.js
@@ -1,4 +1,4 @@
-import { QrReader } from 'react-qr-reader';
+import { QrReader } from 'react-qr-reader-es6';
 import React, { useState } from 'react';
 
 export default function QRScanner({ onScan }) {
